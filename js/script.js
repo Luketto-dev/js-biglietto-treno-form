5fb1924f1645b6c77c6d50436647db7254590c9f
Luketto-dev <luca.francesconi1995@gmail.com>
2022-05-20 10:35:36 +0200

    correzione post-lezione.

--- js/script.js
+++ js/script.js
@@ -10,16 +10,22 @@ let buttoncancel = document.getElementById("cancel")
 
 let error = document.querySelector(".error")
 
-let datiValidi = true;
+let divSectionTicket = document.querySelector(".section-ticket");
 
 
 
 buttonSubmit.addEventListener("click", function(){
 
+    console.log("sono nell'evento")
+    let datiValidi = true;
+
+    // controllo se i kilometri sono < 1 oppure > di 499
     if (kilometri.value < 1 || kilometri.value > 499) {
         datiValidi = false
         error.innerHTML = "inserisci kilometraggio corretto (min:1 max:499)"
     }
+
+    // controllo se il nome non è sia un numero e che non sia vuoto
     if (!isNaN(nomeCognome.value || nomeCognome.value == "")) {
         datiValidi = false
         error.innerHTML = "inserisci un nome e un cognome validi"
@@ -33,15 +39,14 @@ buttonSubmit.addEventListener("click", function(){
 
 
         if (eta.value == "min") {
-            scontoApplicato = .2;
+            scontoApplicato = 0.2;
             offering = "biglietto minorenni"
 
         }else if (eta.value == "over65") {
-            scontoApplicato = .4;
+            scontoApplicato = 0.4;
             offering = "biglietto over-65"
         }
 
-        divSectionTicket = document.querySelector(".section-ticket");
         divSectionTicket.classList.add("d-block");
         divSectionTicket.classList.remove("d-none");
 
@@ -71,6 +76,8 @@ buttonSubmit.addEventListener("click", function(){
         nomeCognome.value = "";
         kilometri.value = "";
 
+    }else{
+
     }
 
 });
@@ -80,4 +87,6 @@ buttoncancel.addEventListener("click", function(){
     if (divSectionTicket.classList.contains("d-block")) {
         divSectionTicket.classList.add("d-none");
     }
+
+    error.innerHTML = "";
 })
